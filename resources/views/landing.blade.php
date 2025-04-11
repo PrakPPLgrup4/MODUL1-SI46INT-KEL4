c4a2c045f034ee9d33547ec22a24e8ac8ea3ade7
FarizRm666 <farizrifqiaiz@gmail.com>
2025-04-11 10:48:22 +0700

    landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <img class="logo" src="images/logo.png" alt="logo">
+      <nav>
+        <ul class="nav_links">
+          <li><a href="#">Journal</a></li>
+          <li><a href="#">Appointment</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Chat</a></li>
+        </ul>
+      </nav>
+      <a class="lgnbutton" href="#">Log in</a>
+    </header>
+    
+    <script src="navIndex.js" async defer></script>
+
+    
+    <div class="hero">
+      <div class="hero_content">
+        <h1>Explore General Counseling Topics</h1>
+        <div class="topic">
+            <div class="topic1"></div>
+            <div class="topic2"></div>
+            <div class="topic3"></div>
+            <div class="topic4"></div>
+            <div class="topic5"></div>
+            <div class="topic6"></div>
+        </div>
+        <div class="hero_text">
+            <p>
+            Stress isn’t always harmful, but prolonged stress can affect your health. It often stems from new experiences or situations beyond your control. While stress is a natural part of life, finding healthy ways to cope can make a significant difference in your well-being.
+            </p>
+
+            <div class="see_more">
+                 <a href="#">See more ></a>
+            </div>
+        </div>
+      </div>
+
+      <div class="hero_index">
+          <img src="images/doctor1.png">
+      </div>
+    </div>
+    
+    
+    <div class="pentool">
+
+      <div class="pentool_index">
+        <img src="images/Pentool.png">
+      </div>
+
+        <div class="pentool_content">
+          <div class="pentool_h1">
+            <h1>Start Your Own Journaling Adventure </h1>
+          </div>
+          
+          <div class="pentool_h2">
+            <h2>Start your own journaling for better self</h2>
+          </div>
+          
+          <div class="pentool_text">
+            <p>Stress isn’t always harmful, but prolonged stress can affect your health. It often stems from new experiences or situations beyond your control. While stress is a natural part of life, finding healthy ways to cope can make a significant difference in your well-being.</p>
+          </div>
+
+          <div class="pentool_button">
+            <a href="#">Start journaling</a>
+          </div>  
+        </div>
+    </div>
+
+    <div class="test">
+
+      <div class="circle1">
+        <img src="images/Circle.png">
+      </div>
+
+      <div class="circle2">
+        <img src="images/Circle.png">
+      </div>
+
+      <div class="test_text">
+        <h1>Mental Health Test</h1>
+        <h2>Express check, instant results.</h2>
+      </div>
+      
+      <div class="test_container">
+        <div class="test_image1">
+          <img src="images/Stress.png">
+          <div class="test_content1">
+            <h3>Stress</h3>
+            <p>Results of the PHQ-9 test adaptation to check and measure symptoms of Stress.</p>
+            <a href="#">Test</a>
+          </div>  
+        </div>
+
+        <div class="test_image2">
+          <img src="images/Anxiety.png">
+          <div class="test_content2">
+            <h3>Anxiety</h3>
+            <p>Results of the PHQ-9 test adaptation to check and measure symptoms of Anxiety.</p>
+            <a href="#">Test</a>
+          </div>
+        </div>
+
+        <div class="test_image3">
+          <img src="images/Depression.png">
+          <div class="test_content3">
+            <h3>Depression</h3>
+            <p>Results of the PHQ-9 test adaptation to check and measure symptoms of depression.</p>
+            <a href="#">Test</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="psychiatrist">
+      <h1>Our Psychiatrist</h1>
+
+      <div class="psy_container">
+        <div class="psy_profile1">
+          <div>
+            <img src="images/psy2.png">
+          </div>
+          <h3>Dr. John Doe</h3>
+          <p>Psychiatrist</p>
+        </div>
+
+        <div class="psy_profile2">
+          <div>
+            <img src="images/psy1.png">
+          </div>
+          <h3>Dr. Jane Smith</h3>
+          <p>Psychiatrist</p>
+        </div>
+
+        <div class="psy_profile3">
+          <div>
+            <img src="images/psy3.png">
+          </div>
+          <h3>Dr. Emily Johnson</h3>
+          <p>Psychiatrist</p>
+        </div>
+      </div>
+        <div class="psy_button">
+          <a href="#">See All Our Psychiatrist</a>
+        </div> 
+    </div>
+
+
+    <div class= "news">
+      <h1>Read about mental health</h1>
+      <h2>Tips & info blablablalbalbal</h2>
+
+      <div class= "news_container">
+        <div class= "news_content">
+          <img src="images/news.png">
+          <div class="news_text">
+            <h3>7 Alasan kenapa menjadi pengangguran itu buruk bagi kesehatan mental</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla par</p>
+          </div>
+        </div>
+
+
+        <div class= "news_content">
+          <img src="images/news.png">
+          <div class="news_text">
+            <h3>7 Alasan kenapa menjadi pengangguran itu buruk bagi kesehatan mental</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla par</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="footer_bg">
+        <img src="images/PSYLO GRAPHY.png">
+        <div class="footer_text">
+            <div class="book_footer">
+                <h1>Consult Now</h1>
+                <hr>
+                <div class="footer_opt">
+                    <a href="{{ url('appointments') }}">Chat With Psychiatrist</a>
+                </div>
+            </div>
+            <div class="discover_footer">
+                <h1>Discover Us</h1>
+                <hr>
+                <div class="footer_opt">
+                    <a href="#about_us">About Us</a>
+                    <a href="{{ url('doctors') }}" >Our Psychiatrist</a>
+                </div>
+            </div>
+            <div class="contact_footer">
+                <h1>Contact Us</h1>
+                <hr>
+                <div class="footer_opt">
+                  <a href="tel:1500115">14022</a>
+                  <a href="mailto:cs@telkomedika.co.id">cs@psylo.co.id</a>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="line"></div>
+    </footer>
+  </body>
+
+
+
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landing');})->name('views.landing');
