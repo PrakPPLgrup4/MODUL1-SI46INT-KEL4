3539d95fccca2dda42113c5c6748efc7335c2616
Owybong <owy0107@gmail.com>
2025-04-15 12:58:22 +0700

    commit branch

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <a href="/home" class="logo-link">
+      <img class="logo" src="images/psylography.svg" alt="logo">
+    </a>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="#">Journal</a></li>
+            <li><a href="#">Appointment</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Chat</a></li>
+        </ul>
+    </nav>
+    <a class="lgnbutton" href="#">Log in</a>
+</header>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PsyciController;
+use App\Http\Controllers\JournalController;
+
+Route::get('/', function () {
+    return view('landing');})->name('views.landing');
+
+//homepage
+route::get('/home', [HomeController::class, 'home'])->name('views.Homepage');
+
+//symmptom
+Route::get('/symptom', function () {
+    return view('symptom'); //ini gw masih bingung routing - / awal tuh buat yg di akses dari webnya - controller itu kau harus buat untuk setiap views yg ada - name tuh buat ngebalikin halaman mana yg mau di tampilin
+})->name('views.symptom');
+
+//symptom content
+Route::get('/symptomcontent1', function(){
+    return view('symptomcontent1');
+})->name('view.symptomcontent1');
+
+//journal
+// Route::get('/journal', [JournalController::class, 'index'])->name('views.journal');
+// Route::get('/journal/create', [JournalController::class, 'create'])->name('User.create');
+// Route::post('/journal', [JournalController::class, 'store'])->name('journal.store');
+
+//journall
+Route::get('/journal', [JournalController::class, 'index'])->name('views.journal');
+Route::get('/journal/create', [JournalController::class, 'create'])->name('User.create');
+Route::post('/journal', [JournalController::class, 'store'])->name('journal.store');
+Route::get('/journal/{id}/edit', [JournalController::class, 'edit'])->name('journal.edit');
+Route::put('/journal/{id}', [JournalController::class, 'update'])->name('journal.update');
+Route::delete('/journal/{id}', [JournalController::class, 'destroy'])->name('journal.destroy');
+
+//psychiatry
+route::get('/psyci', [PsyciController::class, 'psyci'])->name('views.psyci');
