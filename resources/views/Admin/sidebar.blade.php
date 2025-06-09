ec426fe499f77d340254573fb9cb55b50f565fbc
Aditya Dharma <>
2025-06-10 06:56:19 +0700

    no message

--- resources/views/Admin/sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-<aside class="sidebar">
-  <ul>
-    <li><a href="#">User Account</a></li>
-    <li><a href="{{ route('admin.psychs') }}">Psychiatric Profile</a></li>
-    <li><a href="#">Symptoms</a></li>
-    <li><a href="{{ route('admin.quiz.index') }}">Quiz</a></li>
-  </ul>
-</aside>
